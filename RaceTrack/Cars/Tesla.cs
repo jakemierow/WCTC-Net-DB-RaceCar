9c20367395c18f3a861ae9eb467f30be3ffbc172
Jake Mierow <jake.mierow@icloud.com>
2020-03-09 21:15:40 -0500

    implemented new car and driver

---
+++ RaceTrack/Cars/Tesla.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class Tesla : RaceCar
+    {
+        public Tesla()
+        {
+            Name = "Tesla";
+            TopSpeed = 125;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} revs their engine!");
+        }
+
+    }
+    }
+
---
+++ RaceTrack/Drivers/Jake.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class Jake : Driver
+    {
+        public Jake(RaceCar car) : base(car)
+        {
+            Name = "Jake";
+            SkillLevel = 15;
+
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+    }
+}
